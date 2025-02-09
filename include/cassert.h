579e95772c34c8bf48ae59bcad60d29fd51aadc7
qirunzeng <qirunzeng@gmail.com>
2025-02-09 12:28:52 +0800

    assert

--- include/cassert.h
+++ include/cassert.h
@@ -1,13 +1,14 @@
-#ifndef _CASSERT_H_
-#define _CASSERT_H_
+#ifndef MY_ASSERT_H_
+#define MY_ASSERT_H_
 
+#include <exception>
 
 namespace my {
-    #define cassert(x) if (!(x)) { \
-        printf("Assertion failed at %s:%d\n", __FILE__, __LINE__); \
+    #define my_assert(x) if (!(x)) { \
+        throw("Assertion failed at %s:%d\n", __FILE__, __LINE__); \
         exit(1); \
     }
 }
 
 
-#endif // _CASSERT_H_
+#endif // MY_ASSERT_H_
