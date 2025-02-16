6a182c1d7ca3b44fd12b67b11255043f4bb23599
qirunzeng <qirunzeng@gmail.com>
2025-02-16 23:32:02 +0800

    Adding Segment Tree

--- include/segTree.h
+++ include/segTree.h
@@ -5,11 +5,10 @@
  * Description:
  */
 
-
 #ifndef _SEGTREE_H_ 
 #define _SEGTREE_H_
 
-#include "vector.h"
+#include <vector>
 
 namespace my {
     class segTree {
@@ -25,19 +24,95 @@ namespace my {
         node(int n, int m, int a) : num(n), mul(m), add(a) {}
     };
 
-    vector<node> nodes;
-    vector<int> nums;
+    std::vector<node> nodes;
+    std::vector<int> nums;
     size_t size;
 
     void makeTree(int l, int r, int p) {
         if (l == r) {
-            nodes[p] = {nums[l], 0, 0};
+            nodes[p] = {nums[l], 1, 0};
             return ;
         }
         int mid = l + ((r-l)>>1);
         makeTree(l, mid, p<<1);
         makeTree(mid+1, r, p<<1|1);
-        nodes[p] = {nodes[p<<1].num + nodes[p<<1|1].num, 0, 0};
+        nodes[p] = {nodes[p<<1].num + nodes[p<<1|1].num, 1, 0};
+    }
+
+    int getSum(int l, int r, int p, int L, int R) {
+        if (l <= L && r >= R) {
+            return nodes[p].num;
+        }
+        int mid = L + ((R-L)>>1);
+        int res = 0;
+
+        nodes[p<<1].num = nodes[p<<1].num * nodes[p].mul + nodes[p].add * (mid-L+1);
+        nodes[p<<1|1].num = nodes[p<<1|1].num * nodes[p].mul + nodes[p].add * (R-mid);
+        nodes[p<<1].mul *= nodes[p].mul;
+        nodes[p<<1|1].mul *= nodes[p].mul;
+        nodes[p<<1].add = nodes[p<<1].add * nodes[p].mul + nodes[p].add;
+        nodes[p<<1|1].add = nodes[p<<1|1].add * nodes[p].mul + nodes[p].add;
+        nodes[p].mul = 1;
+        nodes[p].add = 0;
+
+        if (l <= mid) {
+            res += getSum(l, r, p<<1, L, mid);
+        }
+        if (r > mid) {
+            res += getSum(l, r, p<<1|1, mid+1, R);
+        }
+        return res;
+    }
+
+    void add(int l, int r, int incr, int p, int L, int R) {
+        if (l <= L && r >= R) {
+            nodes[p].num += incr * (R-L+1);
+            nodes[p].add += incr;
+            return ;
+        }
+        int mid = L + ((R-L)>>1);
+        nodes[p<<1].num = nodes[p<<1].num * nodes[p].mul + nodes[p].add * (mid-L+1);
+        nodes[p<<1|1].num = nodes[p<<1|1].num * nodes[p].mul + nodes[p].add * (R-mid);
+        nodes[p<<1].mul *= nodes[p].mul;
+        nodes[p<<1|1].mul *= nodes[p].mul;
+        nodes[p<<1].add = nodes[p<<1].add * nodes[p].mul + nodes[p].add;
+        nodes[p<<1|1].add = nodes[p<<1|1].add * nodes[p].mul + nodes[p].add;
+        nodes[p].mul = 1;
+        nodes[p].add = 0;
+
+        if (l <= mid) {
+            add(l, r, incr, p<<1, L, mid);
+        }
+        if (r > mid) {
+            add(l, r, incr, p<<1|1, mid+1, R);
+        }
+        nodes[p].num = nodes[p<<1].num + nodes[p<<1|1].num;
+    }
+
+    void mul(int l, int r, int times, int p, int L, int R) {
+        if (l <= L && r >= R) {
+            nodes[p].num *= times;
+            nodes[p].mul *= times;
+            nodes[p].add *= times;
+            return ;
+        }
+        int mid = L + ((R-L)>>1);
+        nodes[p<<1].num = nodes[p<<1].num * nodes[p].mul + nodes[p].add * (mid-L+1);
+        nodes[p<<1|1].num = nodes[p<<1|1].num * nodes[p].mul + nodes[p].add * (R-mid);
+        nodes[p<<1].mul *= nodes[p].mul;
+        nodes[p<<1|1].mul *= nodes[p].mul;
+        nodes[p<<1].add = nodes[p<<1].add * nodes[p].mul + nodes[p].add;
+        nodes[p<<1|1].add = nodes[p<<1|1].add * nodes[p].mul + nodes[p].add;
+        nodes[p].mul = 1;
+        nodes[p].add = 0;
+
+        if (l <= mid) {
+            mul(l, r, times, p<<1, L, mid);
+        }
+        if (r > mid) {
+            mul(l, r, times, p<<1|1, mid+1, R);
+        }
+        nodes[p].num = nodes[p<<1].num + nodes[p<<1|1].num;
     }
 
     public:
@@ -52,7 +127,7 @@ namespace my {
             makeTree(1, n, 1);
         }
 
-        segTree(vector<int> arr) : size(arr.size()) {
+        segTree(std::vector<int> arr) : size(arr.size()) {
             nodes.resize(size<<2);
             nums.resize(size+1);
             for (int i = 0; i < size; ++i) {
@@ -60,8 +135,23 @@ namespace my {
             }
             makeTree(1, size, 1);
         }
+
+        int getSum(int l, int r) {
+            return getSum(l, r, 1, 1, size);
+        }
+
+        void add(int l, int r, int incr) {
+            add(l, r, incr, 1, 1, size);
+        }
+
+        void mul(int l, int r, int times) {
+            mul(l, r, times, 1, 1, size);
+        }
+
+        void set(int pos, int val) {
+            add(pos, pos, val - nums[pos]);
+        }
     };
 }
-
 
 #endif // _SEGTREE_H_
